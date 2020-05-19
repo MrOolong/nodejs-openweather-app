47846bd1a744199c409e93f06709037cbfd65b66
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2020-05-19 12:04:32 -0400

    parsed json from body to show only temperature and city name

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
-  }
+	let weather = JSON.parse(body)
+
+	let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+console.log(message);}
+
 });
